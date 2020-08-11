f29b7103b6c638552f22f1ab57459346ca8b7c4a
Puneeth996 <puneethreddy12@gmail.com>
2020-08-11 13:24:29 -0400

    Dictinory HARDCODED

--- app/src/main/java/com/example/firebase/activity/DashboardActivity.java
+++ app/src/main/java/com/example/firebase/activity/DashboardActivity.java
@@ -1,6 +1,7 @@
 package com.example.firebase.activity;
 
 import android.content.Intent;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.appcompat.BuildConfig;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
--- app/src/main/java/com/example/firebase/activity/DictionaryActivity.java
+++ app/src/main/java/com/example/firebase/activity/DictionaryActivity.java
@@ -1,35 +1,90 @@
 package com.example.firebase.activity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+
 import com.example.firebase.R;
+import com.example.firebase.adapter.DictionaryListAdapter;
+import com.example.firebase.models.Dictionary;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DictionaryActivity extends AppCompatActivity {
+
+    private List<Dictionary> dictionaryArrayList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private DictionaryListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
         Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Dictionary");
         setSupportActionBar(toolbar);
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(DictionaryActivity.this);
-        alertDialogBuilder
-                .setMessage("This Features is Available Coming soon")
-                .setCancelable(false)
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
-                    }
-                });
+        recyclerView = (RecyclerView) findViewById(R.id.dictionary_words);
 
-        alertDialogBuilder.show();
+        mAdapter = new DictionaryListAdapter(this, dictionaryArrayList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareDictionaryData();
+
+
+    }
+
+    private void prepareDictionaryData() {
+
+        Dictionary dictionary = new Dictionary("accompany");
+        dictionaryArrayList.add(dictionary);
+
+        dictionary = new Dictionary("accurate");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("accountant");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("baggage");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("bankrupt");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("dawn");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("earlobe");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("happy");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("knee");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("major");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("negligence");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("negligent");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("obsessive");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("painter");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("panic");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("raise");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("rarely");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("tax");
+        dictionaryArrayList.add(dictionary);
+        dictionary = new Dictionary("teammate");
+        dictionaryArrayList.add(dictionary);
+
     }
 }
